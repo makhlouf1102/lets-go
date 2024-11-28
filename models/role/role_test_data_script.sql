9611fcafe716a4117dc6d9f18f44105e61220df4
makhlouf1102 <makhlouf.hennine.1@ens.etsmtl.ca>
2024-11-28 12:19:14 -0500

    test failing bug fixed

--- models/role/role_test_data_script.sql
+++ models/role/role_test_data_script.sql
@@ -1,8 +1,3 @@
--- Insert test users
-INSERT INTO user (user_id, username, email, password, first_name, last_name)
-VALUES ('testuser1', 'testuser1', 'test1@example.com', 'password1', 'Test1', 'User1');
-
--- Insert test sessions
-INSERT INTO session (session_id, user_id, refreshToken)
-VALUES ('testsession1', 'testuser1', 'refreshtoken1'),
-       ('testsession2', 'testuser1', 'refreshtoken2');
+INSERT INTO role (role_id, name) VALUES ('uuid1', 'Admin');
+INSERT INTO role (role_id, name) VALUES ('uuid2', 'User');
+INSERT INTO role (role_id, name) VALUES ('uuid3', 'Guest');
