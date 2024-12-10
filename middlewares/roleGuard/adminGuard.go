ab6c87be7c6a8b44e0c5559956a410cf8f3c4fda
makhlouf1102 <makhlouf.hennine.1@ens.etsmtl.ca>
2024-12-09 19:10:54 -0500

    role guard added

--- middlewares/roleGuard/adminGuard.go
+++ middlewares/roleGuard/adminGuard.go
@@ -1,27 +1,38 @@
 package logger_middleware
 
 import (
-	"log"
 	"net/http"
-	"time"
+	"slices"
 )
 
 type AdminGuar struct {
 	handler http.Handler
 }
 
-// ServeHTTP handles the request by passing it to the real
-// handler and logging the request details
 func (a *AdminGuar) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	// var userRole []string
-	// if protectedData, ok := r.Context().Value("protected_data").(token.TokenClaim); !ok {
-	// 	http.Error(w, "server error", http.StatusInternalServerError)
-	// 	return
-	// }
-	// userRole = protectedData["userRoles"].([]string)
-	start := time.Now()
+	protectedData, ok := r.Context().Value("protected_data").(map[string]interface{})
+	if !ok {
+		http.Error(w, "server error: invalid protected data type", http.StatusInternalServerError)
+		return
+	}
+
+	userRoles, exists := protectedData["userRoles"]
+	if !exists {
+		http.Error(w, "server error: userRoles key missing", http.StatusInternalServerError)
+		return
+	}
+
+	roles, ok := userRoles.([]string)
+	if !ok {
+		http.Error(w, "server error: userRoles has invalid type", http.StatusInternalServerError)
+		return
+	}
+
+	if !slices.Contains(roles, "Admin") {
+		http.Error(w, "server error: You are not allowed use this feature", http.StatusForbidden)
+		return
+	}
 	a.handler.ServeHTTP(w, r)
-	log.Printf("%s %s %v", r.Method, r.URL.Path, time.Since(start))
 }
 
 // NewLogger constructs a new Logger middleware handler
