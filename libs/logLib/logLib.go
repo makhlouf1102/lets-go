2da70b0324aecb20b9a940f1975960708d7d1b10
makhlouf1102 <makhlouf.hennine.1@ens.etsmtl.ca>
2025-01-02 23:00:19 -0500

    adding a function to log the errors

--- libs/logLib/logLib.go
+++ libs/logLib/logLib.go
@@ -1,2 +1,13 @@
 package loglib
 
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func LogError(text string, err error) {
+	formatedString := fmt.Sprintf("%s : %v \n", text, err)
+	log.Println(formatedString)
+	os.WriteFile("logs.txt", []byte(formatedString), 0666)
+}
