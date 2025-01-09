413b553a346e1a396e0efabca50541f8f65a89cb
makhlouf1102 <makhlouf.hennine.1@ens.etsmtl.ca>
2025-01-09 10:32:19 -0500

    updating log lib

--- libs/logLib/logLib.go
+++ libs/logLib/logLib.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 )
 
 func LogError(text string, err error) {
 	formatedString := fmt.Sprintf("%s : %v \n", text, err)
 	log.Println(formatedString)
+	formatedString = fmt.Sprintf("%s:%s: %s", time.DateOnly, time.TimeOnly, formatedString)
 	os.WriteFile("logs.txt", []byte(formatedString), 0666)
 }
