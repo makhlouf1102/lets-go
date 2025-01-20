73beb583a40f66c9cb94b6fe69fe6046bf201334
makhlouf1102 <makhlouf.hennine.1@ens.etsmtl.ca>
2025-01-19 20:37:18 -0500

    updating data to inject into the database

--- database/scripts/production_inserts.sql
+++ database/scripts/production_inserts.sql
@@ -9,4 +9,7 @@ INSERT INTO problem (problem_id, title, description, difficulty) VALUES
 
 INSERT INTO user VALUES('6f2651cc-e02c-451d-8531-72f43c78f99d','mak','mak@mak.com','$2a$14$uN0QQIYsvmnteDQ3ORU4nuFazGUtjLuIVTM0R49AdHLoJuTc/PFGa','mak','mak');
 INSERT INTO user_role VALUES('f1c38740-9bed-4869-819e-217231059ccf','6f2651cc-e02c-451d-8531-72f43c78f99d','User');
-INSERT INTO user_role VALUES('123e4567-e89b-12d3-a456-426614174002','6f2651cc-e02c-451d-8531-72f43c78f99d','Admin');
+
+INSERT INTO user VALUES('6f2651cc-e02c-451d-8531-72f43c78f98d','admin','admin@admin.com','$2a$14$uN0QQIYsvmnteDQ3ORU4nuFazGUtjLuIVTM0R49AdHLoJuTc/PFGa','admin','admin');
+INSERT INTO user_role VALUES('f1c38740-9bed-4869-819e-217231059caf','6f2651cc-e02c-451d-8531-72f43c78f98d','User');
+INSERT INTO user_role VALUES('123e4567-e89b-12d3-a456-426614174012','6f2651cc-e02c-451d-8531-72f43c78f98d','Admin');
