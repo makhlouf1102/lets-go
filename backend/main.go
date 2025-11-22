f3b5d1da870c808039f21fbaa2e9ee4c2301001c
makhlouf1102 <makhlouf.hennine.1@ens.etsmtl.ca>
2025-11-22 17:59:09 -0500

    feat: add problems route

--- backend/main.go
+++ backend/main.go
@@ -22,6 +22,32 @@ func main() {
 		})
 	})
 
+	r.GET("/problems", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "all problems",
+			"data": []Problem{
+				{
+					ID:          "1",
+					Title:       "Problem 1",
+					Description: "Description 1",
+					Difficulty:  "Easy",
+				},
+				{
+					ID:          "2",
+					Title:       "Problem 2",
+					Description: "Description 2",
+					Difficulty:  "Medium",
+				},
+				{
+					ID:          "3",
+					Title:       "Problem 3",
+					Description: "Description 3",
+					Difficulty:  "Hard",
+				},
+			},
+		})
+	})
+
 	if err := r.Run(":8080"); err != nil {
 		log.Fatal("failed to run server:", err)
 	}
