1fdd86b1efed920d2cddd4a58f84e12f205263b6
makhlouf1102 <makhlouf.hennine.1@ens.etsmtl.ca>
2025-11-22 17:58:24 -0500

    feat: add Problem struct

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,19 @@
-package main 
+package main
+
 import (
-	"net/http"
 	"log"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
+
+type Problem struct {
+	ID          string `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Difficulty  string `json:"difficulty"`
+}
+
 func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
@@ -12,7 +22,7 @@ func main() {
 		})
 	})
 
-	if err:= r.Run(":8080"); err != nil {
-		log.Fatal("failed to run server: %v", err)
+	if err := r.Run(":8080"); err != nil {
+		log.Fatal("failed to run server:", err)
 	}
 }
