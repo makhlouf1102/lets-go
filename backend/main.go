252c12af6d3319231fa1de3f545e0511fcea322e
makhlouf1102 <makhlouf.hennine.1@ens.etsmtl.ca>
2025-11-27 15:21:12 -0500

    Update problems response structure in main.go

--- backend/main.go
+++ backend/main.go
@@ -15,6 +15,27 @@ type Problem struct {
 	Difficulty  string `json:"difficulty"`
 }
 
+var problems = []Problem{
+	{
+		ID:          "1",
+		Title:       "Problem 1",
+		Description: "Description 1",
+		Difficulty:  "Easy",
+	},
+	{
+		ID:          "2",
+		Title:       "Problem 2",
+		Description: "Description 2",
+		Difficulty:  "Medium",
+	},
+	{
+		ID:          "3",
+		Title:       "Problem 3",
+		Description: "Description 3",
+		Difficulty:  "Hard",
+	},
+}
+
 func main() {
 	r := gin.Default()
 	r.Use(cors.Default())
@@ -27,26 +48,7 @@ func main() {
 	r.GET("/problems", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "all problems",
-			"data": []Problem{
-				{
-					ID:          "1",
-					Title:       "Problem 1",
-					Description: "Description 1",
-					Difficulty:  "Easy",
-				},
-				{
-					ID:          "2",
-					Title:       "Problem 2",
-					Description: "Description 2",
-					Difficulty:  "Medium",
-				},
-				{
-					ID:          "3",
-					Title:       "Problem 3",
-					Description: "Description 3",
-					Difficulty:  "Hard",
-				},
-			},
+			"data":    problems,
 		})
 	})
 
