12e570d149e05ccde0cc0740fccf63fc28417cd6
makhlouf1102 <makhlouf.hennine.1@ens.etsmtl.ca>
2025-11-29 22:57:26 -0500

    feat: update backend main logic

--- backend/main.go
+++ backend/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"io"
 	"log"
 	"net/http"
 	"strconv"
@@ -14,6 +17,11 @@ type Problem struct {
 	Title       string `json:"title"`
 	Description string `json:"description"`
 	Difficulty  string `json:"difficulty"`
+}
+
+type RunCodeRequest struct {
+	SourceCode string `json:"source_code"`
+	LanguageID int    `json:"language_id"`
 }
 
 var problems = []Problem{
@@ -85,9 +93,52 @@ func getProblemById(router *gin.Engine) *gin.Engine {
 }
 
 func runCode(router *gin.Engine) *gin.Engine {
+	// js id is 63
+	// 	{
+	//   "source_code": "#include <stdio.h>\n\nint main(void) {\n  char name[10];\n  scanf(\"%s\", name);\n  printf(\"hello, %s\n\", name);\n  return 0;\n}",
+	//   "language_id": 4,
+	//   "stdin": "world"
+	// }
 	router.POST("/code/run", func(c *gin.Context) {
+		var req RunCodeRequest
+		if err := c.BindJSON(&req); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"message": "invalid request",
+			})
+			return
+		}
+
+		var buf bytes.Buffer
+		if err := json.NewEncoder(&buf).Encode(req); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"message": "failed to encode request",
+			})
+			return
+		}
+
+		// send a request to the judge
+		resp, err := http.Post("http://server:2358/submissions/?wait=true", "application/json", &buf)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"message": "failed to run code",
+				"error":   err.Error(),
+			})
+			return
+		}
+		defer resp.Body.Close()
+
+		respBody, err := io.ReadAll(resp.Body)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"message": "failed to read response",
+				"error":   err.Error(),
+			})
+			return
+		}
+
 		c.JSON(http.StatusOK, gin.H{
 			"message": "code run",
+			"data":    respBody,
 		})
 	})
 	return router
