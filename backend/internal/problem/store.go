93696f2d10aa9eafce8cd341930afce48f3990d5
makhlouf1102 <makhlouf.hennine.1@ens.etsmtl.ca>
2025-12-19 14:52:59 -0500

    feat: Add problem store with methods to retrieve a problem and its associated tests.

--- backend/internal/problem/store.go
+++ backend/internal/problem/store.go
@@ -6,8 +6,9 @@ import (
 )
 
 type Store interface {
-	GetProblem(ctx context.Context, problemID int64) (Problem, error)
-	GetTests(ctx context.Context, problemID int64) ([]TestProblem, error)
+	GetProblem(ctx context.Context, problemID int64) (*Problem, error)
+	// ListProblems(ctx context.Context) ([]Problem, error)
+	ListTests(ctx context.Context, problemID int64) ([]TestProblem, error)
 }
 
 type ProblemStore struct {
@@ -18,10 +19,31 @@ func NewProblemStore(db *sql.DB) Store {
 	return &ProblemStore{db: db}
 }
 
-func (ps *ProblemStore) GetProblem(ctx context.Context, problemID int64) (Problem, error) {
-	return Problem{}, nil
+func (ps *ProblemStore) GetProblem(ctx context.Context, problemID int64) (*Problem, error) {
+	row := ps.db.QueryRowContext(ctx, "SELECT * FROM problems WHERE id = $1", problemID)
+
+	var p Problem
+	if err := row.Scan(&p.ID, &p.Name, &p.Description, &p.Template, &p.Difficulty); err != nil {
+		return nil, err
+	}
+
+	return &p, nil
 }
 
-func (ps *ProblemStore) GetTests(ctx context.Context, problemID int64) ([]TestProblem, error) {
-	return []TestProblem{}, nil
+func (ps *ProblemStore) ListTests(ctx context.Context, problemID int64) ([]TestProblem, error) {
+	rows, err := ps.db.QueryContext(ctx, "SELECT * FROM tests WHERE problem_id = $1", problemID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var tests []TestProblem
+	for rows.Next() {
+		var t TestProblem
+		if err := rows.Scan(&t.ID, &t.ProblemID, &t.Input, &t.Output); err != nil {
+			return nil, err
+		}
+		tests = append(tests, t)
+	}
+	return tests, nil
 }
