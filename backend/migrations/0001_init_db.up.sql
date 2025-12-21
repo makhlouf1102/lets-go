1c2bb6033328029db3bac8487e6f49d715ddee38
makhlouf1102 <makhlouf.hennine.1@ens.etsmtl.ca>
2025-12-21 15:23:35 -0500

    chore(db): add IF NOT EXISTS to initial migration and update foreign key with cascade

--- backend/migrations/0001_init_db.up.sql
+++ backend/migrations/0001_init_db.up.sql
@@ -1,4 +1,4 @@
-CREATE TABLE problems (
+CREATE TABLE IF NOT EXISTS problems (
     id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     description TEXT NOT NULL,
@@ -6,7 +6,7 @@ CREATE TABLE problems (
     difficulty VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE tests (
+CREATE TABLE IF NOT EXISTS tests (
     id SERIAL PRIMARY KEY,
     problem_id INTEGER NOT NULL,
     input TEXT NOT NULL,
@@ -14,4 +14,16 @@ CREATE TABLE tests (
 );
 
 -- create reference between problem and test
-ALTER TABLE tests ADD FOREIGN KEY (problem_id) REFERENCES problems(id);
+DO $$
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1
+        FROM pg_constraint
+        WHERE conname = 'tests_problem_id_fkey'
+    ) THEN
+        ALTER TABLE tests 
+        ADD CONSTRAINT tests_problem_id_fkey 
+        FOREIGN KEY (problem_id) REFERENCES problems(id) 
+        ON DELETE CASCADE;
+    END IF;
+END $$;
